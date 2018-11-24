ffa80e8f87b99a8f332a940eb0813af7c2098e94
Samuel Baillargeon <samuel.baillargeon.1@ulaval.ca>
2018-11-23 19:44:56 -0500

    Attempt

--- src/main/java/ca/ulaval/glo4002/chatter/domain/SequentialNameRoomFactory.java
+++ src/main/java/ca/ulaval/glo4002/chatter/domain/SequentialNameRoomFactory.java
@@ -5,25 +5,29 @@ import java.util.Map;
 
 public class SequentialNameRoomFactory implements RoomFactory {
 
-    private static Map<String, Integer> nameCache = new HashMap<>();
+    private static final Map<String, Integer> nameCache = new HashMap<>();
 
-    public synchronized Room createRoom(String roomName) {
+    public Room createRoom(String roomName) {
         return new Room(roomName, createUniqueName(roomName));
     }
 
     private String createUniqueName(String name) {
-        if (!nameCache.containsKey(name)) {
-            nameCache.put(name, 1);
-            return name;
-        } else {
-            Integer currentCount = nameCache.get(name);
-            nameCache.put(name, currentCount + 1);
-            return name + "_" + currentCount;
+        synchronized (nameCache) {
+            if (!nameCache.containsKey(name)) {
+                nameCache.put(name, 1);
+                return name;
+            } else {
+                Integer currentCount = nameCache.get(name);
+                nameCache.put(name, currentCount + 1);
+                return name + "_" + currentCount;
+            }
         }
     }
 
     void clearCache() {
-        nameCache.clear();
+        synchronized (nameCache) {
+            nameCache.clear();
+        }
     }
 
 }
